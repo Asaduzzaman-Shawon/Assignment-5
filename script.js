3381dbcf417ab89313679eda1bfa3c854bc75287
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-06 18:24:57 +0600

    Donate history added

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ document.getElementById('history-btn').addEventListener('click', function(){
     const a = show('history-section')
 })
 
+document.getElementById('donate-flood').addEventListener('click', function(){
+   const mainBalance =  getElementByText('main-balance');
+   const donateBalance = getElementByInput('flood-ammount');
+   const floodAccount = getElementByText('flood');
+   const newCollect = floodAccount + donateBalance;
+   document.getElementById('flood').innerText = newCollect;
+   document.getElementById('main-balance').innerText = mainBalance - donateBalance;
+   const dateAndTime = new Date()
+   const historyAdd = document.createElement('div')
+   historyAdd.innerHTML = `
+        <div class="w-[80%] mx-auto mt-6 p-3 border-gray-700 border-[1px] rounded-lg">
+                    <p class="font-bold text-xl mb-3">${donateBalance} Taka is Donated for Flood at Noakhali, Bangladesh</p>
+                    <p>${dateAndTime}</p>
+                </div>
+   `
+   const addedLocation = document.getElementById('history-section');
+   addedLocation.append(historyAdd)
+})
+
+
--- utilities.js
+++ utilities.js
@@ -3,3 +3,15 @@ function show(id){
     document.getElementById('donate-section').classList.add('hidden')
     document.getElementById(id).classList.remove('hidden')
 }
+
+function getElementByText(id){
+    const Balance = document.getElementById(id).innerText;
+    const BalanceNumber = parseFloat(Balance);
+    return BalanceNumber;
+}
+
+function getElementByInput(id){
+    const inputBalance = document.getElementById(id).value;
+    const inputBalanceNumber = parseFloat(inputBalance);
+    return inputBalanceNumber;
+}
